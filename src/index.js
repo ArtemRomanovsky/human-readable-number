64381a686b71c29fe0a72cf915b7d5110a433bb6
ArtemRomanovsky <cheferin1@gmail.com>
2021-08-15 10:38:30 +0300

    add solution (need to fix issue with ' ')

--- src/index.js
+++ src/index.js
@@ -1,3 +1,124 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    let hundreds = '';
+    let tens = '';
+    let units = '';
+    let finalResultHundreds = '';
+    let finalResultTens = '';
+    let finalResultUnits = '';
+    let finalResult = '';
+    const stringNumber = number.toString();
+
+    if (stringNumber.length >= 3 && stringNumber.length < 4) {
+        hundreds = parseInt(stringNumber[0]);
+        tens = parseInt(stringNumber[1]);
+        units = parseInt(stringNumber[2]);
+    }
+    if (stringNumber.length >= 2 && stringNumber.length < 3) {
+        tens = parseInt(stringNumber[0]);
+        units = parseInt(stringNumber[1]);
+    }
+    if (stringNumber.length === 1) {
+        units = parseInt(stringNumber[0]);
+    }
+
+    if (hundreds > 0 && hundreds < 10) {
+        finalResultHundreds = ((getRightNumber(hundreds)) + ' hundred');
+    }
+
+    if (tens > 1 && tens < 10) {
+        finalResultTens = getRightTens(tens);
+        finalResultUnits = getRightNumber(units);
+        if (units === 0) {
+            finalResultTens = getRightTens(tens);
+            finalResultUnits = '';
+        }
+    }
+
+    if (tens === 1) {
+        finalResultTens = getValueFromTenToNineteen(units);
+    }
+
+    if (tens === 0) {
+        finalResultTens = '';
+        finalResultUnits = getRightNumber(units);
+        if (units === 0) {
+            finalResultUnits = '';
+        }
+
+    }
+
+    if (finalResultTens === '' && finalResultUnits === '') {
+        finalResult = finalResultHundreds;
+    }
+
+    if (finalResultUnits === '') {
+        finalResult = finalResultHundreds + ' ' + finalResultTens;
+    }
+    finalResult = finalResultHundreds + ' ' + finalResultTens + ' ' + finalResultUnits;
+    return finalResult;
 }
+
+function getRightNumber(number) {
+    switch (number) {
+        case 0: return 'zero';
+        case 1: return 'one';
+        case 2: return 'two';
+        case 3: return 'three';
+        case 4: return 'four';
+        case 5: return 'five';
+        case 6: return 'six';
+        case 7: return 'seven';
+        case 8: return 'eight';
+        case 9: return 'nine';
+        // case 10: return 'десять';
+        default: return null;
+    }
+}
+
+function getRightTens(number) {
+    switch (number) {
+        case 0: return 'zero';
+        // case 1: return 'one';
+        case 2: return 'twenty';
+        case 3: return 'thirty';
+        case 4: return 'forty';
+        case 5: return 'fifty';
+        case 6: return 'sixty';
+        case 7: return 'seventy';
+        case 8: return 'eighty';
+        case 9: return 'ninety';
+        // case 10: return 'десять';
+        default: return null;
+    }
+
+}
+
+function getValueFromTenToNineteen(number) {
+    switch (number) {
+        case 0: return 'ten';
+        case 1: return 'eleven';
+        case 2: return 'twenty';
+        case 3: return 'thirteen';
+        case 4: return 'fourteen';
+        case 5: return 'fifteen';
+        case 6: return 'sixteen';
+        case 7: return 'seventeen';
+        case 8: return 'eighteen';
+        case 9: return 'nineteen';
+        // case 10: return 'десять';
+        default: return null;
+    }
+
+}
+
+
+/*
+   1. парсить полученное число на цифры разрядов (toString-> array[i+1]->reverse)
+   2. получить строку для каждого разряда, добавить слова разрядов (сотни,десятки)
+   3. объединить строки в одну
+   4. вернуть результат
+
+*/
+
+
+
